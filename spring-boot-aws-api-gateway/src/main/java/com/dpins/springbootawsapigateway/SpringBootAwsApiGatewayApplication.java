4afdf832bc76cb772e3259a87e0906bf12ecdeb5
Dhruvesh Patel <dmpatel.1412@gmail.com>
2020-09-16 21:34:27 +0100

    Added Spring boot proect for aws api gateway

---
+++ spring-boot-aws-api-gateway/src/main/java/com/dpins/springbootawsapigateway/SpringBootAwsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.dpins.springbootawsapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsApiGatewayApplication.class, args);
+	}
+
+}
---
+++ spring-boot-aws-api-gateway/src/test/java/com/dpins/springbootawsapigateway/SpringBootAwsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpins.springbootawsapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
